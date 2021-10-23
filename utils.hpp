5001ee5c77685ba35d9dbc0d557557df06363ff0
royS <72430659+sohamroy19@users.noreply.github.com>
2021-10-24 00:58:41 +0530

    Changed reading convention

--- utils.hpp
+++ utils.hpp
@@ -379,8 +379,8 @@ void ParseData::readData(string data, std::vector<float> &adjMat)
         int first_entry = std::stoi(line_data.at(0));
         int sec_entry = std::stoi(line_data.at(1));
 
-        adjMat[(_data_dims.at(0) * (first_entry - 1)) + (sec_entry - 1)] = -stof(line_data.at(2));
-        adjMat[(_data_dims.at(0) * (sec_entry - 1)) + (first_entry - 1)] = -stof(line_data.at(2));
+        adjMat[(_data_dims.at(0) * (first_entry - 1)) + (sec_entry - 1)] = stof(line_data.at(2));
+        adjMat[(_data_dims.at(0) * (sec_entry - 1)) + (first_entry - 1)] = stof(line_data.at(2));
         // include the above line if only upper triangle of adjacencies is given
 
         //std::cout << adjMat[_data_dims.at(0)*(line_data.at(0) - 1) + (line_data.at(1) - 1)] << std::endl;
